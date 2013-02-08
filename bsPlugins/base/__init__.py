46fe0621654ff102fdf268ad5f22d4924a4b70c3
Jacques Rougemont <jacques.rougemont@epfl.ch>
2013-02-08 15:59:42 +0100

    details

--- bsPlugins/base/__init__.py
+++ bsPlugins/base/__init__.py
@@ -8,16 +8,19 @@ class OperationPlugin():
     def __init__(self):
         self.files={}
 
-    def temporary_path(self, fname, ext):
+    def temporary_path(self, fname='', ext=''):
         tmpf = NamedTemporaryFile(prefix=fname, suffix="."+ext, 
                                   dir="./", delete=False)
         tmpf.close()
         return tmpf.name
 
-
     def new_file(self, fname, descr):
         self.files[fname] = descr
         print "%s: %s" %(fname,descr)
+
+    def display_time(self):
+        return 1
+
 
 class Multi():
     pass
