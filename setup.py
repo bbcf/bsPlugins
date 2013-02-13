d4c42b865c69c823759882ca5a85f893736fe11f
Yohan Jarosz <yohanjarosz@yahoo.fr>
2013-02-13 16:42:56 +0100

    comment setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
-from distutils.core import setup
+# from distutils.core import setup
 
-setup(
-        name            =   'bsPlugins',
-        version         =   '0.0.1',
-        description     =   '',
-        long_description=   open('README.md').read(),
-        license         =   'GNU General Public License 3.0',
-        url             =   'http://bbcf.epfl.ch/bbcflib',
-        author          =   'EPFL BBCF',
-        author_email    =   'webmaster.bbcf@epfl.ch',
-        install_requires=   ['bbcflib'],
-        classifiers     =   ['Topic :: Scientific/Engineering :: Bio-Informatics'],
-        packages        =   ['bsPlugins',
-                             'bsPlugins.tw0',
-                             'bsPlugins.base'
-                            ],
-    )
+# setup(
+#         name            =   'bsPlugins',
+#         version         =   '0.0.1',
+#         description     =   '',
+#         long_description=   open('README.md').read(),
+#         license         =   'GNU General Public License 3.0',
+#         url             =   'http://bbcf.epfl.ch/bbcflib',
+#         author          =   'EPFL BBCF',
+#         author_email    =   'webmaster.bbcf@epfl.ch',
+#         install_requires=   ['bbcflib'],
+#         classifiers     =   ['Topic :: Scientific/Engineering :: Bio-Informatics'],
+#         packages        =   ['bsPlugins',
+#                              'bsPlugins.tw0',
+#                              'bsPlugins.base'
+#                             ],
+#     )
