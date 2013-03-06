d4f86c52c001868a0f9910659bcdc806b139f635
Jacques Rougemont <jacques.rougemont@epfl.ch>
2013-03-06 17:06:48 +0100

    multi-list topgo

--- bsPlugins/TopGo.py
+++ bsPlugins/TopGo.py
@@ -3,16 +3,12 @@ from bbcflib.btrack import track
 from bbcflib import genrep
 import rpy2.robjects as robjects
 import rpy2.robjects.numpy2ri as numpy2ri
+import os, tarfile
 
 mart_map = [("GRCh37.p5",'hg19'), ("NCBIM37",'mm9'), ("EF3","sacCer2"), 
             ("BDGP5.25",'dm3'),("Zv9",'zv9')]
 
-ensembl_url = "sep2011.archive.ensembl.org"
-biomart = "ENSEMBL_MART_ENSEMBL"
-attribute_go = "go_id"
-attribute_gene = "external_gene_id"
-filter_go = "with_go"
-
+default_path = "/mnt/common/epfl/share"
 
 class TopGoForm(BaseForm):
     gene_list = twf.FileField(label='Genes: ',
@@ -30,72 +26,54 @@ meta = {'version': "1.0.0",
 
 in_parameters = [{'id': 'gene_list', 'type': 'userfile', 'required': True},
                  {'id': 'assembly', 'type': 'assembly'}]
-out_parameters = [{'id': 'TopGO_table', 'type': 'txt'},
+out_parameters = [{'id': 'TopGO_table_tar', 'type': 'file'},
+                  {'id': 'TopGO_plots_tar', 'type': 'file'},
+                  {'id': 'TopGO_table', 'type': 'txt'},
                   {'id': 'TopGO_plots', 'type': 'pdf'}]
 
 
 class TopGoPlugin(OperationPlugin):
 
     info = {
-       'title': 'TopGo',
-       'description': 'Makes a GO analysis on a list of Ensembl IDs',
-       'path': ['Features', 'TopGo'],
-       'output': TopGoForm,
-       'in': in_parameters,
-       'out': out_parameters,
-       'meta': meta,
-       }
+        'title': 'TopGo',
+        'description': 'Makes a GO analysis on a list of Ensembl IDs',
+        'path': ['Features', 'TopGo'],
+        'output': TopGoForm,
+        'in': in_parameters,
+        'out': out_parameters,
+        'meta': meta,
+        }
+
 
     def __call__(self, **kw):
-       assembly_id = kw.get('assembly') or None
-       filename = kw.get('gene_list')
-       assert os.path.exists(str(filename)), "File not found: '%s'" % filename
-       pdf = self.temporary_path(fname='TopGO_plots.pdf')
-       table = self.temporary_path(fname='TopGO_tables.txt')
-       robjects.r("""
-id_set = scan("%s",what=character())
-pdf("%s",paper="a4",height=8,width=11)
-output = "%s"
-library(biomaRt)
-library(topGO)
-ensembl = useMart("%s",host="%s")
-lsds = listDatasets(ensembl)
-dataset = lsds$dataset[which(lsds$version == "%s")]
-ensembl = useDataset(as.character(dataset),mart=ensembl)
-attr1 = "%s"
-attr2 = "%s"
-filt = "%s"
-genome = getBM(attr=c("ensembl_gene_id",attr1,attr2),
-  filter=c(filt,"biotype"),
-  values=list(TRUE,"protein_coding"),mart=ensembl)
-gene2GO = split(genome[,attr1],genome[,"ensembl_gene_id"])
-I = which(!duplicated(genome[,"ensembl_gene_id"]))
-genome = data.frame(gene_name=genome[I,attr2],row.names=genome[I,"ensembl_gene_id"])
-allGenes = row.names(genome)
-genome[which(!nchar(genome[,1])),1] = allGenes[which(!nchar(genome[,1]))]
-tab = list()
-geneList = factor(as.numeric(!is.na(match(allGenes,id_set))))
-names(geneList) = allGenes
-append = FALSE
-for (ontol in c("BP","CC","MF")) {
-    data = new("topGOdata",description=ontol,ontology=ontol,
-               allGenes=geneList,annot=annFUN.gene2GO,gene2GO=gene2GO)
-    result = list(classicFisher=runTest(data, statistic="fisher", algo="classic"),
-                  elimFisher=runTest(data, statistic="fisher", algo="elim"))
-    tab = GenTable(data, classicFisher=result$classicFisher, 
-                   elimFisher=result$elimFisher, 
-                   orderBy="elimFisher", ranksOf="elimFisher", topNodes=10)
-    showSigOfNodes(data,score(result$elimFisher), first=10, useInfo="all")
-    tab = cbind(tab,genes=sapply(tab$GO.ID, function(x){
-             paste(sort(genome[unlist(genesInTerm(data,x)),1]),collapse=', ')
-               }))
-    write(c("",ontol,""),file=output,append=append)
-    append = TRUE
-    write.table(tab,file=output,quote=F,sep="\t",row.names=F,append=append)
-}
-dev.off()
-         """ % (filename,pdf,table,biomart,ensembl_url,assembly_id,
-                attribute_go,attribute_gene,filter_go))
-       self.new_file(pdf, 'TopGO_plots')
-       self.new_file(table, 'TopGO_table')
-       return self.display_time()
+        assembly_id = kw.get('assembly') or None
+        filename = kw.get('gene_list')
+        assert os.path.exists(str(filename)), "File not found: '%s'" %filename
+        script_path = kw.get("script_path",default_path)
+        pdf = self.temporary_path(fname='TopGO_plots.pdf')
+        table = self.temporary_path(fname='TopGO_tables.txt')
+       
+        robjects.r("""
+source("%s/TopGo.R")
+out = multi_topGo("%s","%s","%s","%s")
+"""%(script_path,filename,assembly_id,pdf,table))
+
+        pdf_list = list(robjects.r('out')[0])
+        table_list = list(robjects.r('out')[1])
+        if len(pdf_list) > 1:
+            tar_pdf_name = self.temporary_path('TopGO_plots.tgz')
+            tar_pdf = tarfile.open(tar_pdf_name, "w:gz")
+            [tar_pdf.add(f) for f in pdf_list]
+            tar_pdf.close()
+            
+            tar_table_name = self.temporary_path(fname='TopGO_tables.tgz')
+            tar_table = tarfile.open(tar_table_name, "w:gz")
+            [tar_table.add(f) for f in table_list]
+            tar_table.close()
+            self.new_file(tar_pdf_name, 'TopGO_plots_tar')
+            self.new_file(tar_table_name, 'TopGO_table_tar')
+        else:
+            self.new_file(pdf,'TopGO_plots')
+            self.new_file(table,'TopGO_table')
+                
+        return self.display_time()
