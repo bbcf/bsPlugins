7824d138459b156e0918849af21ee789b9c1a45c
benmoham <sara.benmohammed@epfl.ch>
2013-04-04 15:47:12 +0300

    Create loga2.py

---
+++ bsPlugins/loga2.py
@@ -0,0 +1,9 @@
+from bsPlugins import *
+from bbcflib.bFlatMajor import stream as gm_stream
+from bbcflib import btrack as track
+from bbcflib import genrep
+from bbcflib.btrack import track
+from bbcflib.bFlatMajor import common
+from bbcflib.bFlatMajor.common import split_field,apply
+import re
+import math #log2, log10, ...
