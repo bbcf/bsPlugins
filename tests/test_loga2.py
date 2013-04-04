e9e2d7b227d15c1e0faafe9a7814315fa99dda0c
benmoham <sara.benmohammed@epfl.ch>
2013-04-04 15:44:52 +0300

    Update test_loga2.py

--- tests/test_loga2.py
+++ tests/test_loga2.py
@@ -8,7 +8,7 @@ class Test_loga2Plugin(TestCase):
         self.plugin = loga2Plugin()
     def test_with_signals(self):
         self.plugin(**{'track':[path+'CTCF_deconv.sql'],
-  'assembly':'mm9', "format":"sql",'function':'sqrt'})
+        'assembly':'mm9', "format":"sql",'function':'sqrt'})
         #self.plugin(**{'track':[path+'test1.bedGraph',path+'CTCF_deconv.sql', path+'test2.bedGraph',path+"CTCF_rev.bw", path+'WT50.bedGraph', path+'KO50.bedGraph'],
         #'assembly':'mm9', "format":"",'function':'log2'})
     def tearDown(self):
