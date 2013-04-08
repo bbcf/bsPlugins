eaec2964c4aa410305eb49028b6f93aa605c9685
Julien Delafontaine <julien.delafontaine@epfl.ch>
2013-04-08 14:04:19 +0200

    update OperationPlugin (old Loga2)

--- bsPlugins/Operation.py
+++ bsPlugins/Operation.py
@@ -1,15 +1,12 @@
 from bsPlugins import *
-from bbcflib.bFlatMajor import stream as gm_stream
-from bbcflib import btrack as track
 from bbcflib import genrep
 from bbcflib.btrack import track
 from bbcflib.bFlatMajor import common
-from bbcflib.bFlatMajor.common import split_field,apply
-import re
-import math #log2, log10, sqrt, ...
-# import toscawidget2 modules in order to build forms
+import math
 import tw2.forms as twf
-class Loga2Form(BaseForm):    #form
+
+
+class OperationForm(BaseForm):
     class SigMulti(Multi):
         label='Signals: '
         track = twf.FileField(label=' ',
@@ -27,6 +24,8 @@ class Loga2Form(BaseForm):    #form
         options=genrep.GenRep().assemblies_available(),
         help_text='Reference genome')
     submit = twf.SubmitButton(id="submit", value="Submit")
+
+
 meta = {'version': "1.0.0",
         'author': "BBCF",
         'contact': "webmaster-bbcf@epfl.ch"}
@@ -36,13 +35,14 @@ in_parameters = [{'id': 'track', 'type': 'track', 'required': True},    # input 
                 {'id': 'format', 'type': 'format'}]
 out_parameters = [{'id': 'output', 'type': 'file'}]    # output parameters
 
-class Loga2Plugin(OperationPlugin):
-    description = """ Operation on a track."""
+
+class OperationPlugin(BasePlugin):
+    description = """Apply a numeric transformation to the track scores - such as logarithm or square root."""
     info = {
-        'title': 'log2 or log10 or square root',
+        'title': 'Operation',
         'description': description,
-        'path': ['Signal', 'Log2 - Log10 - sqrt'],
-        'output': loga2Form,
+        'path': ['Signal', 'Operation'],
+        'output': OperationForm,
         'in': in_parameters,
         'out': out_parameters,
         'meta': meta,
@@ -55,20 +55,18 @@ class Loga2Plugin(OperationPlugin):
                 return math.log10( x) ;
             elif kw.get('function')=="sqrt":
                 return math.sqrt(x) ;
-            elif kw.get('function')=="":
-                return math.log( x,2) ;
+            else:
+                return math.log(x) ;
+
         assembly_id = kw.get('assembly') #or None
         assembly = genrep.Assembly(assembly_id)
-        chrmeta = assembly.chrmeta
         length=len( kw.get('track') )    # number of tracks
         for i in range( length  ) :
             tname = kw['track'][i]
             tinput = track(tname, chrmeta=kw.get('assembly'))
             (filepath, filename) = os.path.split(tname)    # ( path, name of one track)
             (shortname, extension) = os.path.splitext(filename)    # (name of one track (without path) , extension)
-            modif= kw.get('function')
-            if modif =="":
-                modif="log2"
+            modif= kw['function']
             if not isinstance(tinput, (tuple,list)):
                 tinput = [tinput]
             for trackFILE in tinput:
@@ -80,8 +78,8 @@ class Loga2Plugin(OperationPlugin):
                         #apply method function to the score field trackFILE
                         #create and write in the new file
                         t1_name.write(common.apply(trackFILE.read(),'score',method ) ,mode='write')
-                        t1_name.close() #close the output track file
-                        trackFILE.close() #close the input track file
+                        t1_name.close()
+                        trackFILE.close()
                     if kw.get('format')!="":
                         output2_name = self.temporary_path( shortname +'_'+kw.get('assembly')+'_'+ modif +'.'+ kw.get('format') )
                         t2_name = track(output2_name,chrmeta=assembly.chrmeta)
--- tests/test_Operation.py
+++ tests/test_Operation.py
@@ -1,16 +1,20 @@
 from unittest2 import TestCase, skip
-from bsPlugins.Loga2 import Loga2Plugin
+from bsPlugins.Operation import OperationPlugin
 from bbcflib.btrack import track
 import os
+
 path = './testing_files/'
-class Test_Loga2Plugin(TestCase):
+
+class Test_OperationPlugin(TestCase):
     def setUp(self):
-        self.plugin = Loga2Plugin()
+        self.plugin = OperationPlugin()
+
     def test_with_signals(self):
         self.plugin(**{'track':[path+'CTCF_deconv.sql'],
         'assembly':'mm9', "format":"sql",'function':'sqrt'})
         #self.plugin(**{'track':[path+'test1.bedGraph',path+'CTCF_deconv.sql', path+'test2.bedGraph',path+"CTCF_rev.bw", path+'WT50.bedGraph', path+'KO50.bedGraph'],
         #'assembly':'mm9', "format":"",'function':'log2'})
+
     def tearDown(self):
         for f in os.listdir('.'):
             if f.startswith('tmp'):
