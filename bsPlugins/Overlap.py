2c872cb2f278d9899adfc4c3dff6d113ac0c0839
Marion Leleu <mleleu@frt.el.vital-it.ch>
2013-04-09 15:49:44 +0200

    Overlap.py: remove choice of genes, promoters, exon (will be done in another plugin)

--- bsPlugins/Overlap.py
+++ bsPlugins/Overlap.py
@@ -9,24 +9,12 @@ ftypes = [(0,'gene bodies'), (1,'gene promoters'), (2,'exons'), (3,'custom uploa
 
 class OverlapForm(BaseForm):
     child = twd.HidingTableLayout()
-    feature_type = twd.HidingSingleSelectField(label='Input type: ',
-       options=ftypes, prompt_text=None,
-       mapping={ftypes[-1][0]: ['features'],
-                            1: ['upstream_1','downstream_1']},
-       help_text='Choose a feature set or upload your own',
-       validator=twc.Validator(required=True))
-    features = twf.FileField(label='Custom file: ',
+    features = twf.BsFileField(label='Features file: ',
         help_text='Upload your own file',
-        validator=twf.FileValidator(required=True))
-    filter_type = twd.HidingSingleSelectField(label='Filter type: ',
-       options=ftypes, prompt_text=None,
-       mapping={ftypes[-1][0]: ['filter'],
-                            1: ['upstream_2','downstream_2']},
-       help_text='Choose a filter set or upload your own',
-       validator=twc.Validator(required=True))
-    filter = twf.FileField(label='Custom Filter file: ',
-                           help_text='Upload your own file',
-                           validator=twf.FileValidator(required=True))
+        validator=twf.BsFileFieldValidator(required=True))
+    filter = twf.BsFileField(label='Filter file: ',
+        help_text='Upload your own file',
+        validator=twf.BsFileFieldValidator(required=True))
     format = twf.SingleSelectField(label='Output format: ',
         options=["txt","bed","sql","bedGraph","bigWig"],
         validator=twc.Validator(required=True),
@@ -34,22 +22,6 @@ class OverlapForm(BaseForm):
     assembly = twf.SingleSelectField(label='Assembly: ',
         options=genrep.GenRep().assemblies_available(),
         help_text='Reference genome')
-    upstream_1 = twf.TextField(label='Promoter upstream distance: ',
-        validator=twc.IntValidator(required=True),
-        value=prom_up_def,
-        help_text='Size of promoter upstream of TSS')
-    downstream_1 = twf.TextField(label='Promoter downstream distance: ',
-        validator=twc.IntValidator(required=True),
-        value=prom_down_def,
-        help_text='Size of promoter downstream of TSS')
-    upstream_2 = twf.TextField(label='Promoter upstream distance: ',
-        validator=twc.IntValidator(required=True),
-        value=prom_up_def,
-        help_text='Size of promoter upstream of TSS')
-    downstream_2 = twf.TextField(label='Promoter downstream distance: ',
-        validator=twc.IntValidator(required=True),
-        value=prom_down_def,
-        help_text='Size of promoter downstream of TSS')
     submit = twf.SubmitButton(id="submit", value="Submit")
 
 
@@ -58,15 +30,9 @@ meta = {'version': "1.0.0",
         'contact': "webmaster-bbcf@epfl.ch"}
 
 in_parameters = [{'id': 'filter', 'type': 'userfile', 'required': True},
-                 {'id': 'filter_type', 'type': 'list'},
-                 {'id': 'feature_type', 'type': 'list'},
                  {'id': 'features', 'type': 'userfile', 'required': True},
                  {'id': 'format', 'type': 'text'},
                  {'id': 'assembly', 'type': 'assembly'},
-                 {'id': 'upstream_1', 'type': 'int'},
-                 {'id': 'downstream_1', 'type': 'int'},
-                 {'id': 'upstream_2', 'type': 'int'},
-                 {'id': 'downstream_2', 'type': 'int'}]
 out_parameters = [{'id': 'filtered', 'type': 'track'}]
 
 
@@ -82,12 +48,8 @@ class OverlapPlugin(BasePlugin):
         'meta': meta,
         }
     def __call__(self, **kw):
-        feature_type = int(kw.get('feature_type', 0))
-        assembly_id = kw.get('assembly')
-        filter = kw.get("filter")
-        assert os.path.exists(str(filter)), "File file not found: '%s'." % filter
-        filter = os.path.abspath(filter)
         # Set assembly
+        assembly_id = kw.get('assembly')
         chrmeta = "guess"
         if assembly_id:
             assembly = genrep.Assembly(assembly_id)
@@ -97,41 +59,19 @@ class OverlapPlugin(BasePlugin):
         elif not(feature_type == 3):
             raise ValueError("Please specify an assembly")
         # Set features track
-        if feature_type == 0: features = genes
-        elif feature_type == 1:
-            prom_pars = {'before_start': int(kw.get('upstream_1') or prom_up_def),
-                         'after_start': int(kw.get('downstream_1') or prom_down_def),
-                         'on_strand': True}
-            features = lambda c: neighborhood(genes(c), **prom_pars)
-        elif feature_type == 2: features = exons
-        elif feature_type == 3:
-            assert os.path.exists(str(kw.get('features'))), "Features file not found: '%s'"%kw.get("features")
-            _t = track(kw.get('features'), chrmeta=chrmeta)
-            chrmeta = _t.chrmeta
-            features = _t.read
-        else: raise ValueError("Take feature_type in %s."%ftypes)
+        assert os.path.exists(str(kw.get('features'))), "Features file not found: '%s'"%kw.get("features")
+        features = track(kw.get('features'), chrmeta=chrmeta or None )
+        chrmeta = _t.chrmeta
         # Set filter track
-        if filter_type == 0: filter = genes
-        elif filter_type == 1:
-            prom_pars = {'before_start': int(kw.get('upstream_2') or prom_up_def),
-                         'after_start': int(kw.get('downstream_2') or prom_down_def),
-                         'on_strand': True}
-            filter = lambda c: neighborhood(genes(c), **prom_pars)
-        elif filter_type == 2: filter = exons
-        elif filter_type == 3:
-            assert os.path.exists(str(kw.get('filter'))), "Filter file not found: '%s'"%kw.get("filter")
-            _t = track(kw.get('filter'), chrmeta=chrmeta)
-            chrmeta = _t.chrmeta
-            features = _t.read
-        else: raise ValueError("Take filter_type in %s."%ftypes)
+        assert os.path.exists(str(kw.get('filter'))), "Filter file not found: '%s'"%kw.get("filter")
+        filter = track(kw.get('filter'), chrmeta=chrmeta or None)
         # Main
-        tfilter = track(filter,chrmeta=chrmeta)
-        format = kw.get('format',tfilter.format)
+        format = kw.get('format',filter.format)
         output = self.temporary_path(fname='filtered.'+format)
-        tout = track(output, format, fields=tfilter.fields,
+        tout = track(output, format, fields=filter.fields,
                      chrmeta=chrmeta, info={'datatype':'qualitative'})
         for chrom in chrmeta:
-            tout.write(overlap(features(chrom),tfilter.read(chrom)), chrom=chrom,clip=True)
+            tout.write(overlap(features.read(chrom),filter.read(chrom)), chrom=chrom,clip=True)
         tout.close()
         self.new_file(output, 'filtered')
         return self.display_time()
