c52cd66a4096073d6c564cf5a4099cb9edebf28f
Marion Leleu <mleleu@frt.el.vital-it.ch>
2013-04-17 16:34:24 +0200

    Table2Tracks.py:tar output files

--- tests/test_Table2Tracks.py
+++ tests/test_Table2Tracks.py
@@ -8,10 +8,23 @@ class Test_Table2TracksPlugin(TestCase):
     def setUp(self):
         self.plugin = Table2TracksPlugin()
 
-    def test_A(self):
-        self.plugin(**{'tableFile':path+'Dup_vs_Ctrl_resSelectedFrags_fromSmoothed_KCTD13_part.txt', 'id_columns':'6,7,9,10', 'assembly':'hg19', 'format':'bedGraph'})
+    def test_single(self):
+        #self.plugin(**{'tableFile':path+'Dup_vs_Ctrl_resSelectedFrags_fromSmoothed_KCTD13_part.txt', 'id_columns':'6,7,9,10', 'assembly':'hg19', 'format':'bedGraph'})
+        self.plugin(**{'tableFile':path+'Dup_vs_Ctrl_resSelectedFrags_fromSmoothed_KCTD13_part.txt', 'id_columns':'6', 'assembly':'hg19', 'format':'bedGraph'})
+        print("in test_single")
         print(self.plugin.output_files)
-        self.assertEqual(len(self.plugin.output_files[0]),4)
+        print(self.plugin.output_files[0])
+        print(self.plugin.tmp_files)
+        self.assertEqual(len(self.plugin.tmp_files),1)
+
+
+    def test_multi(self):
+        self.plugin(**{'tableFile':path+'Dup_vs_Ctrl_resSelectedFrags_fromSmoothed_KCTD13_part.txt', 'id_columns':'6,7,9,10', 'assembly':'hg19', 'format':'bedGraph'})
+        print("in test_multi")
+        print(self.plugin.output_files)
+        print(self.plugin.output_files[0])
+        print(self.plugin.tmp_files)
+        self.assertEqual(len(self.plugin.tmp_files),5)
 
     def tearDown(self):
         for f in os.listdir('.'):
