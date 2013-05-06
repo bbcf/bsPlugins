e81b2559ec10137e02668efc6dd8f6895d163c1b
Jacques Rougemont <jacques.rougemont@epfl.ch>
2013-05-06 13:51:15 +0200

    improved (?) numeric plugin

--- bsPlugins/NumericOperation.py
+++ bsPlugins/NumericOperation.py
@@ -1,29 +1,25 @@
 from bsPlugins import *
 from bbcflib import genrep
 from bbcflib.btrack import track
-from bbcflib.bFlatMajor.common import score_threshold,apply
-import math
-import tw2.forms as twf
+from bbcflib.bFlatMajor.common import score_threshold, apply
+from math import log10, sqrt, log
+import os, tarfile
+
 class NumericOperationForm(BaseForm):
     class SigMulti(twb.BsMultiple):
         label='Signals: '
         track = twb.BsFileField(label=' ',
-            help_text='Select files (e.g. bedgraph)',
-            validator=twb.BsFileFieldValidator(required=True))
+                                help_text='Select files (e.g. bedgraph)',
+                                validator=twb.BsFileFieldValidator(required=True))
     function =  twf.SingleSelectField(label='Operation: ',
-        prompt_text=None,
-        options=["log2","log10","sqrt"],
-        validator=twc.Validator(required=False),
-        help_text='Select a function')
+                                      prompt_text=None,
+                                      options=["log2","log10","sqrt"],
+                                      validator=twc.Validator(required=False),
+                                      help_text='Select a function')
     format = twf.SingleSelectField(label='Output format: ',
-        prompt_text=None,
-        options=["sql","bedgraph","bigwig","wig"],
-        validator=twc.Validator(required=False),
-        help_text='Output file(s) format, by default: same format as input file(s) format(s)')
-    assembly = twf.SingleSelectField(label='Assembly: ',
-        prompt_text=None,
-        options=genrep.GenRep().assemblies_available(),
-        help_text='Reference genome')
+                                   options=["sql","bedgraph","bigwig","wig"],
+                                   validator=twc.Validator(required=False),
+                                   help_text='Output file(s) format, by default: same format as input file(s) format(s)')
     submit = twf.SubmitButton(id="submit", value="Submit")
 
 
@@ -32,13 +28,14 @@ meta = {'version': "1.0.0",
         'contact': "webmaster-bbcf@epfl.ch"}
 
 in_parameters = [{'id': 'track', 'type': 'track', 'required': True, 'multiple':'SigMulti'},
-                {'id': 'assembly', 'type': 'assembly'},
                 {'id': 'function', 'type': 'function'},
                 {'id': 'format', 'type': 'format'}]
-out_parameters = [{'id': 'converted_track', 'type': 'track'}]
+out_parameters = [{'id': 'converted_track_tar', 'type': 'file'},
+                  {'id': 'converted_track', 'type': 'track'}]
 
 
 class NumericOperationPlugin(BasePlugin):
+
     description = """Apply a numeric transformation to the track scores - such as logarithm or square root."""
     info = {
         'title': 'Numeric Operation',
@@ -49,35 +46,35 @@ class NumericOperationPlugin(BasePlugin):
         'out': out_parameters,
         'meta': meta,
         }
+
+    def log2(x):
+        return math.log(x,2)
+
     def __call__(self, **kw):
-        def filter_track(t):    # the function that is applied to the scores
-            if kw['function']=="sqrt":
-                return score_threshold(t, threshold=0, lower=False, strict=False, fields='score') # score >= 0
-            else:
-                return score_threshold(t, threshold=0, lower=False, strict=True, fields='score') # score > 0
-        def method(x):    # the function that is applied to the scores
-            if kw['function']=="log2":
-                return math.log(x,2) ;
-            elif kw['function']=="log10":
-                return math.log(x,10) ;
-            elif kw['function']=="sqrt":
-                return math.sqrt(x) ;
-        assembly = genrep.Assembly(kw.get('assembly'))
+        func = kw.get('function',"log2")
         l_track = kw.get('SigMulti', {}).get('track',[])
         if not isinstance(l_track, list): l_track = [l_track]
+        outall = []
         for tname in l_track :
-            tinput = track(tname, chrmeta=kw.get('assembly'))
-            (filepath, filename) = os.path.split(tname)
-            (shortname, extension) = os.path.splitext(filename)
-            if  "score" in tinput.fields:
-                if kw['format']=="":
-                    out_name = shortname+'_'+kw['function']+str(extension)
-                else:
-                    out_name = shortname+'_'+kw['function']+'.'+kw['format']
-                output_name = self.temporary_path(out_name)
-                out_track = track(output_name,chrmeta=assembly.chrmeta)
-                out_track.write(apply(filter_track(tinput),'score',method), mode='write')
-                out_track.close()
+            tinput = track(tname)
+            if 'score' not in tinput.fields: continue
+            format = kw.get('format',tinput.format)
+            shortname = os.path.splitext(os.path.basename(tname))[0]
+            out_name = shortname+'_'+func+'.'+format
+            outtrack = self.temporary_path(out_name)
+            out_track = track(output_name,chrmeta=tinput.chrmeta)
+            filtered = score_threshold(tinput, strict=(func[:3] == "log"))
+            out_track.write(apply(filtered,'score',eval(func)), mode='write')
+            out_track.close()
+            outall.append(out_name)
             tinput.close()
-        self.new_file(out_track, 'converted_track')
+        if len(outall) == 1:
+            self.new_file(outall[0], 'converted_track')
+        elif len(outall) > 1:
+            tar_name = self.temporary_path(fname="numeric_operation_out.tgz")
+            tar = tarfile.open(tar_name, "w:gz")
+            [tar.add(f,arcname=os.path.basename(f)) for f in outall]
+            tar.close()
+            self.new_file(tar_name, 'converted_track_tar')
+            
         return self.display_time()
