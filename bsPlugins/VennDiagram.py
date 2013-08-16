334df556cdb0972b21b5575d6db716f74adb0423
Jacques Rougemont <jacques.rougemont@epfl.ch>
2013-08-16 14:00:36 +0200

    simplify Venn diagram

--- bsPlugins/VennDiagram.py
+++ bsPlugins/VennDiagram.py
@@ -1,37 +1,11 @@
 from bsPlugins import *
-from bbcflib.track import track
-from bbcflib.gfminer.common import *
+from bbcflib.track import track, FeatureStream
+from bbcflib.gfminer.common import cobble
 from bbcflib.gfminer.stream import concatenate
 from bbcflib.gfminer.figure import venn
 from bbcflib import genrep
 from itertools import combinations
 import os
-
-
-class VennDiagramForm(BaseForm):
-    child = twd.HidingTableLayout()
-    class SigMulti(Multi):
-        label = "Files: "
-        files = twb.BsFileField(label=' ',
-            help_text='Select your track files',
-            validator=twb.BsFileFieldValidator(required=True))
-    names = twf.TextArea(label='Sample names: ',
-        placeholder="(One name per line)",
-        help_text="Sample names (facultative)" )
-    type = twf.SingleSelectField(label='Type: ',
-        prompt_text=None,
-        options=['coverage %','tag count'],
-        help_text='Output figure format')
-    format = twf.SingleSelectField(label='Format: ',
-        prompt_text=None,
-        options=['png','pdf'],
-        help_text='Output figure format')
-    assembly = twf.SingleSelectField(label='Assembly: ',
-        prompt_text=None,
-        options=genrep.GenRep().assemblies_available(),
-        help_text='Reference genome')
-    submit = twf.SubmitButton(id="submit", value="Submit")
-
 
 meta = {'version': "1.0.0",
         'author': "BBCF",
@@ -39,30 +13,43 @@ meta = {'version': "1.0.0",
 
 in_parameters = [
         {'id':'files', 'type':'track', 'required':True, 'multiple':'SigMulti'},
-        {'id':'names', 'type':'text'},
         {'id':'type', 'type':'list'},
         {'id':'format', 'type':'list'},
-        {'id':'assembly', 'type':'assembly'},
 ]
 out_parameters = [{'id':'venn_diagram', 'type':'file'},
                   {'id':'venn_summary', 'type':'file'}]
 
 
+
+class VennDiagramForm(BaseForm):
+    child = twd.HidingTableLayout()
+    class SigMulti(Multi):
+        label = "Files: "
+        files = twb.BsFileField(label=' ',
+                                help_text='Select your track files',
+                                validator=twb.BsFileFieldValidator(required=True))
+    type = twf.SingleSelectField(label='Type: ',
+                                 prompt_text=None,
+                                 options=['intervals','score'],
+                                 help_text='Output figure format')
+    format = twf.SingleSelectField(label='Format: ',
+                                   prompt_text=None,
+                                   options=['png','pdf'],
+                                   help_text='Output figure format')
+    submit = twf.SubmitButton(id="submit", value="Submit")
+
+
 class VennDiagramPlugin(BasePlugin):
     """Creates a Venn diagram of the proportions of
-total coverage/total tag count due to each of the given tracks.
+total coverage/total score attributed to each of the given tracks.
 
-If the parameter 'type' has the value 'coverage %', the diagram will show the percent
+If the parameter 'type' has the value 'intervals', the diagram will show the percent
 of the genome covered by each possible combination of the input tracks. For instance,
 If tracks A and B are given, it will show the portion covered by A only, B only, or
 A and B (where they intersect).
 
-If it has the value 'tag count', the diagram will show the percent of the total (read)
-count due to each combination of the input tracks, as above.
-
-By default, the graph will display names such as 'A','B',etc. with a legend associating the
-name to its file. But one can also specify the sample names, so that they appear directly
-and without legend on the graph.
+If it has the value 'score', the diagram will show the percent of the total score
+due to each combination of the input tracks, as above.
 
 The output includes the figure of the Venn diagram and a text summary of the different statistics.
 If more than 4 samples are given, no graph is produced, but the text summary still contains
@@ -80,84 +67,66 @@ all the information.
 
     def __call__(self, **kw):
         self.debug(**kw)
-        assembly = genrep.Assembly(kw['assembly'])
         filenames = kw['SigMulti']['files']
         if not isinstance(filenames,(list,tuple)): filenames = [filenames]
         for f in filenames: assert os.path.exists(f), "File not found: %s ." % f
-        tracks = [track(f) for f in filenames]
-        group_names = [n.strip() for n in kw['names'].split('\n')]
-        track_names = group_names if len(group_names) == len(tracks) \
-                      else [chr(i+65) for i in range(len(tracks))] # 'A','B','C',...
-        combn = [combinations(track_names,k) for k in range(1,len(tracks)+1)]
+        tracks = [track(f,chrmeta='guess') for f in filenames]
+        tlabels = [chr(kk+65) for kk in range(len(tracks))]
+        combn = [combinations(tlabels,k+1) for k in range(len(tlabels))]
         combn = ['|'.join(sorted(y)) for x in combn for y in x]
-        cnt = dict(zip(combn,[0]*len(combn)))
-        cumcnt = dict(zip(combn,[0]*len(combn)))
-        cov = dict(zip(combn,[0]*len(combn)))
-        cumcov = dict(zip(combn,[0]*len(combn)))
-        def _f(i): # hack
-            return lambda x:track_names[i]
+        c1 = dict(zip(combn,[0]*len(combn)))
+        c2 = dict(zip(combn,[0]*len(combn)))
         total_cov = 0.0
-        for chrom in assembly.chrmeta:
-            streams = [t.read(chrom) for t in tracks]
-            streams = [duplicate(s,'chr','track_name') for s in streams]
-            streams = [apply(s,'track_name',_f(i)) for i,s in enumerate(streams)]
-            s = concatenate(streams, aggregate={'track_name':lambda x:'|'.join(x)})
-            s = cobble(s,scored=True)
+        _scored = (kw.get('type') == 'score')
+        chromset = set([c for t in tracks for c in t.chrmeta])
+        def _add_label(s,x):
+            _f = s.fields+['track_name']
+            return FeatureStream((y+(x,) for y in s), fields=_f)
+        for chrom in chromset:
+            streams = [_add_label(t.read(chrom),tlabels[n]) for n,t in enumerate(tracks)]
+            s = cobble(concatenate(streams),scored=_scored)
             name_idx = s.fields.index('track_name')
             start_idx = s.fields.index('start')
             end_idx = s.fields.index('end')
-            if 'score' in s.fields:
-                score_idx = s.fields.index('score')
-                _score = lambda x: x[score_idx]
-            else:
-                if kw['type']=='tag count': raise ValueError("'score' field not found.")
-                _score = lambda x: 0
+            if _scored: score_idx = s.fields.index('score')
             for x in s:
-                score = _score(x)
                 length = x[end_idx]-x[start_idx]
                 total_cov += length
                 sub = sorted(list(set(x[name_idx].split('|')))) # avoid 'A|A'
                 cb = [combinations(sub,k) for k in range(1,len(sub)+1)]
                 cb = ['|'.join(sorted(y)) for c in cb for y in c]
-                for c in cb:
-                    cumcnt[c] += score   # 'cumulative', for the plot
-                    cumcov[c] += length
-                cnt['|'.join(sub)] += score  # 'separate', for the stats
-                cov['|'.join(sub)] += length
-        if total_cov == 0:
+                if _scored:
+                    c1['|'.join(sub)] += x[score_idx]
+                    for c in cb: c2[c] += x[score_idx]
+                else:
+                    c1['|'.join(sub)] += length
+                    for c in cb: c2[c] += length
+        if total_cov < 1:
             output = self.temporary_path(fname='venn_summary.txt')
             with open(output,'wb') as summary:
-                summary.write("Empty content (no coverage). Check the chromosome names/assembly.")
+                summary.write("Empty content (no coverage). Check the chromosome names.")
             self.new_file(output, 'venn_summary')
             return
         venn_options = {} # tune it here
         output = self.temporary_path(fname='venn_diagram.'+kw['format'])
-        # Set legend
-        if len(group_names)==len(tracks): legend = None
-        else: legend = dict(zip(track_names,[os.path.basename(f) for i,f in enumerate(filenames)]))
-        if kw['type']=='tag count':
-            for c in cnt:
-                cumcnt[c] = round(cumcnt[c])
-                cnt[c] = cnt[c]
-            if len(tracks) <= 4:
-                venn(cumcnt,legend=legend,options=venn_options,output=output,format=kw['format'])
-        elif kw['type']=='coverage %':
-            for c in cnt:
-                cumcov[c] = round(cumcov[c]/total_cov * 100)
-                cov[c] = cov[c]/total_cov * 100
-            if len(tracks) <= 4:
-                venn(cumcov,legend=legend,options=venn_options,output=output,format=kw['format'])
+        legend = [t.name for t in tracks]
+        if _scored:
+            for c in combn:
+                c2[c] = round(c2[c])
+        else:
+            for c in combn:
+                c2[c] = round((100*c2[c])/total_cov)
+                c1[c] = (100*c1[c])/total_cov
+        if len(tracks) <= 4:
+            venn(c2,legend=legend,options=venn_options,output=output,format=kw['format'])
         self.new_file(output, 'venn_diagram')
         # Text summary
         output = self.temporary_path(fname='venn_summary.txt')
         with open(output,'wb') as summary:
             summary.write("%s\t%s\t%s\n" % ("Group","Coverage", "Cumulative coverage"))
-            if kw['type']=='tag count':
-                for c in sorted(cumcnt.keys(), key=lambda x:(len(x),x)):
-                    summary.write("%s\t%.2f\t%d\n" % (c,cnt[c],cumcnt[c]))
-            elif kw['type']=='coverage %':
-                for c in sorted(cumcnt.keys(), key=lambda x:(len(x),x)):
-                    summary.write("%s\t%.2f %%\t%d %%\n" % (c,cov[c],cumcov[c]))
+            record = "%s\t%.2f\t%d\n" 
+            for c in sorted(combn, key=lambda x:(len(x),x)):
+                summary.write(record%(c,c1[c],c2[c]))
         self.new_file(output, 'venn_summary')
         return self.display_time()
 
