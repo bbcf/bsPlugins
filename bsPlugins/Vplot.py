5bbbbac1356dcf9c4a48fffbdfd483f198d94df0
pjacquet <philippe.jacquet@epfl.ch>
2014-05-02 10:59:43 +0200

    Add options to Vplot

--- bsPlugins/Vplot.py
+++ bsPlugins/Vplot.py
@@ -2,13 +2,20 @@ from bsPlugins import *
 from bbcflib.gfminer.figure import Vplot
 from bbcflib.track import track
 
+nbin_x_def = 500; nbin_y_def = 500
+bandwidth_x_def = 0.1; bandwidth_y_def = 0.1
+
 meta = {'version': "1.0.0",
         'author': "BBCF",
         'contact': "webmaster-bbcf@epfl.ch"}
 
 in_parameters = [{'id': 'bamfiles', 'type': 'bam', 'required': True, 'multiple': 'BamMulti'},
                  {'id': 'features', 'type': 'track'},
-                 {'id': 'linear', 'type': 'boolean'}]
+                 {'id': 'linear', 'type': 'boolean'},
+                 {'id': 'nbins_x', 'type': 'int'},
+                 {'id': 'nbins_y', 'type': 'int'},
+                 {'id': 'bandwidth_x', 'type': 'float'},
+                 {'id': 'bandwidth_y', 'type': 'float'}]
 out_parameters = [{'id': 'Vplot', 'type': 'pdf'}]
 
 
@@ -18,11 +25,27 @@ class VplotForm(BaseForm):
         bamfiles = twb.BsFileField(label=' ',
                                    validator=twb.BsFileFieldValidator(required=True))
     features = twb.BsFileField(label='Features: ',
-                               help_text='Select a feature file (e.g. bed) in which all regions have the same length',
-                               validator=twb.BsFileFieldValidator(required=True))
+                                help_text='Select a feature file (e.g. bed) in which all regions have the same length',
+                                validator=twb.BsFileFieldValidator(required=True))
     linear = twf.CheckBox(label='Linear scale: ',
-                          value=False,
-                          help_text='Plot the mean fragment length in linear scale (default: log scale)')
+                                value=False,
+                                help_text='Plot the mean fragment length in linear scale (default: log scale)')
+    nbin_x = twf.TextField(label='Number of bins along x axis: ',
+                                validator=twc.IntValidator(required=False),
+                                value=nbin_x_def,
+                                help_text='Number of equally spaced grid points for the density estimation (default: 500)')
+    nbin_y = twf.TextField(label='Number of bins along y axis: ',
+                                validator=twc.IntValidator(required=False),
+                                value=nbin_y_def,
+                                help_text='Number of equally spaced grid points for the density estimation (default: 500)')
+    bandwidth_x = twf.TextField(label='Smoothing bandwidth along x axis: ',
+                                validator=twc.IntValidator(required=False),
+                                value=bandwidth_x_def,
+                                help_text='The default smoothing bandwidth is 0.1')
+    bandwidth_y = twf.TextField(label='Smoothing bandwidth along y axis: ',
+                                validator=twc.IntValidator(required=False),
+                                value=bandwidth_y_def,
+                                help_text='The default smoothing bandwidth is 0.1')
     submit = twf.SubmitButton(id="submit", value="Plot")
 
 
@@ -50,12 +73,16 @@ class VplotPlugin(BasePlugin):
         linscale = kw.get('linear',False)
         if isinstance(linscale, basestring):
             linscale = (linscale.lower() in ['1', 'true', 't','on'])
-        if linscale: 
+        if linscale:
             ymin = 0
             log = ''
-        else: 
+        else:
             ymin = 50
             log = 'y'
+        nbin_x = int(kw.get('nbin_x') or nbin_x_def)
+        nbin_y = int(kw.get('nbin_y') or nbin_y_def)
+        bandwidth_x = float(kw.get('bandwidth_x') or bandwidth_x_def)
+        bandwidth_y = float(kw.get('bandwidth_y') or bandwidth_y_def)
         xlab = "Position in window [bp]"
         ylab = "Fragment size [bp]"
         pdf = self.temporary_path(fname='Vplot.pdf')
@@ -81,8 +108,8 @@ class VplotPlugin(BasePlugin):
                         else:
                             X.append(pos-region[1])
             ymax = max(Y)
-            Vplot( X, Y, output=pdf, new=new, last=last, main=bam.name, 
-                   xlab=xlab, ylab=ylab, ylim=(ymin,ymax), log=log )
+            Vplot( X, Y, output=pdf, new=new, last=last, main=bam.name,
+                   xlab=xlab, ylab=ylab, ylim=(ymin,ymax), log=log, nbin=(nbin_x,nbin_y), bandwidth=(bandwidth_x,bandwidth_y) )
             new = False
         self.new_file(pdf, 'Vplot')
         return self.display_time()
