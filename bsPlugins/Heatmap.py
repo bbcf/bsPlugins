4f0599b25dcba02c65c687d89fb818746d9b0603
pjacquet <philippe.jacquet@epfl.ch>
2014-05-26 14:02:58 +0200

    Heatmap is finished

--- bsPlugins/Heatmap.py
+++ bsPlugins/Heatmap.py
@@ -2,6 +2,8 @@ from bsPlugins import *
 from bbcflib.gfminer.figure import heatmap
 from bbcflib.track import track
 from numpy import array, log2, median, abs
+import rpy2.robjects as robjects
+import rpy2.robjects.numpy2ri as numpy2ri
 
 nb_colors_def = 10
 
@@ -46,57 +48,45 @@ class HeatmapPlugin(BasePlugin):
         'meta': meta,
         }
 
-    def heatmapID(M,names,output=None,**kwargs):
-        """Creates a heatmap of the matrix *M* using *rows* as row labels and *columns* as column labels.
-        If either *orderRows* or *orderCols* is True, will cluster accordingly and display a dendrogram."""
-        robjects.r.assign('Mdata',numpy2ri.numpy2ri(M))
-        robjects.r.assign('names',numpy2ri.numpy2ri(names))
-        if orderCols and orderRows:
-            plotopt += ",dendrogram='both',lhei=c(2,10,1,2),lwid=c(1,3),mar=c(2,2),lmat=matrix(c(0,2,0,0,3,1,0,4),ncol=2)"
-        elif orderCols:
-            plotopt += ",Rowv=F,dendrogram='column',lhei=c(2,10,1,2),lwid=c(1),mar=c(2,2),lmat=matrix(c(3,1,2,4),ncol=1)"
-        elif orderRows:
-            plotopt += ",Colv=F,dendrogram='row',lhei=c(10,1,2),lwid=c(1,3),mar=c(2,2),lmat=matrix(c(2,0,3,1,0,4),ncol=2)"
-        else:
-            plotopt += ",Colv=F,Rowv=F,dendrogram='none',lhei=c(10,1,1,2),lwid=c(1),mar=c(2,2),lmat=matrix(c(1,2,3,4),ncol=1)"
-        if kwargs.get('ymin') is not None:
-            robjects.r("ymin=%f" %float(kwargs['ymin']))
-        else:
-            robjects.r("ymin=floor(min(Mdata,na.rm=T))")
-        if kwargs.get('ymax') is not None:
-            robjects.r("ymax=%f" %float(kwargs['ymax']))
-        else:
-            robjects.r("ymax=ceiling(max(Mdata,na.rm=T))")
-        if kwargs.get('nb_colors') is not None:
-            robjects.r("ncol=%i" %kwargs['nb_colors'])
-        else:
-            robjects.r("ncol=10")
-
-        robjects.r("""
-          library(gplots)
-          library(RColorBrewer)
-          myBreaks = seq(ymin,ymax,length.out=ncol+1)
-          myColors = rev(colorRampPalette(brewer.pal(ncol,"RdYlBu"))(ncol))
-    #      myCor = function(x) {as.dist(1-cor(t(x),use="pairwise.complete.ob"))}
-          data = as.matrix(Mdata)
-          h=heatmap.2(data,
-                    col=myColors, trace="none", breaks=myBreaks, #distfun=myCor,
-                    na.rm=TRUE, density.info='none'%s)
-          names = rep('',nrow(data))
-          names[h$rowInd[seq(1,length(h$rowInd),200)]]= match(seq(1,nrow(dmat)),h$rowInd)[h$rowInd[seq(1,length(h$rowInd),200)]]
-          genes = gene_names[h$rowInd]
-          output = h$rowInd""" %plotopt)
-        return output
 
     def __call__(self, **kw):
+
+        def heatmapID(M,names,output1,output2,**kwargs):
+            robjects.r.assign('Mdata',numpy2ri.numpy2ri(M))
+            robjects.r.assign('names',numpy2ri.numpy2ri(names))
+            if kwargs.get('nb_colors') is not None:
+                robjects.r("ncol=%i" %kwargs['nb_colors'])
+            else:
+                robjects.r("ncol=10")
+
+            robjects.r("""
+              library(gplots)
+              library(RColorBrewer)
+              ymin=floor(min(Mdata,na.rm=T))
+              ymax=ceiling(max(Mdata,na.rm=T))
+              myBreaks = seq(ymin,ymax,length.out=ncol+1)
+              myColors = rev(colorRampPalette(brewer.pal(ncol,"RdYlBu"))(ncol))
+        #      myCor = function(x) {as.dist(1-cor(t(x),use="pairwise.complete.ob"))}
+              data = as.matrix(Mdata)
+              h=heatmap.2(data,
+                        col=myColors, trace="none", breaks=myBreaks, #distfun=myCor,
+                        na.rm=TRUE, dendrogram='both', density.info='none')
+              nb_names=ceiling(nrow(data)/30)
+              rownames=rep('',nrow(data))
+              rownames[h$rowInd[seq(1,length(h$rowInd),nb_names)]]=match(seq(1,nrow(data)),h$rowInd)[h$rowInd[seq(1,length(h$rowInd),nb_names)]]
+              n=c(); for (k in 1:length(names)){n[k]=names[[k]]}
+              listnames=n[h$rowInd]""")
+            output1.append(array(robjects.r("rownames")))
+            output2.append(array(robjects.r("listnames")))
+
         table = open(kw.get('table'))
+        col_labels = table.readline().strip("\n").strip("\r").split("\t")[1:]
+        nb_colors = int(kw.get('nb_colors') or nb_colors_def)
         names = []; values = []
         for line in table:
             newline = line.strip("\n").strip("\r").split("\t")
-            name = newline[0]
-            names.append(name)
-            value = [float(newline[i]) for i in range(1,len(newline))]
-            values.append(value)
+            names.append(newline[0])
+            values.append([float(newline[i]) for i in range(1,len(newline))])
         logscale = kw.get('log',False)
         if isinstance(logscale, basestring):
             logscale = (logscale.lower() in ['1', 'true', 't','on'])
@@ -111,14 +101,17 @@ class HeatmapPlugin(BasePlugin):
         if isinstance(make_list, basestring):
             make_list = (make_list.lower() in ['1', 'true', 't','on'])
         if make_list:
-            h=heatmapID(array(values),names)
+            v1 = []; v2 = []
+            heatmapID(array(values),names,v1,v2,nb_colors=nb_colors)
+            names = v1[0].tolist()
+            listnames = v2[0].tolist()
             List_ID = self.temporary_path(fname='List_ID.txt')
             with open(List_ID,"w") as L:
-                L.write("\t".join(names)+"\n")
+                for position, name in enumerate(listnames):
+                    L.write(str(position+1)+"\t"+name+"\n")
             self.new_file(List_ID, 'List')
-        nb_colors = int(kw.get('nb_colors') or nb_colors_def)
-        title = "List of genes"
+        title = table.name
         pdf = self.temporary_path(fname='Heatmap.pdf')
-        heatmap(array(values), output=pdf, rows=names, main=title, nb_colors=nb_colors)
+        heatmap(array(values), output=pdf, rows=names, columns=col_labels, main=title, nb_colors=nb_colors)
         self.new_file(pdf, 'Heatmap')
         return self.display_time()
