b2fe231206f8809cc0d1950496e3a2c7983e8af4
Julien Delafontaine <julien.delafontaine@epfl.ch>
2015-01-20 16:50:26 +0100

    up tests

--- tests/test_Pca.py
+++ tests/test_Pca.py
@@ -8,14 +8,11 @@ class Test_PcaPlugin(TestCase):
     def setUp(self):
         self.plugin = PcaPlugin()
 
-    #def test_pca_select(self):
-    #    self.plugin(**{'table': path+'genes_expression_100_Leanne.txt','columns': "1,2,3"})
+    def test_pca_select(self):
+        self.plugin(**{'table': path+'genes_expression_100_Leanne.txt','columns': "1,2,3,4"})
 
     #def test_pca_all(self):
     #    self.plugin(**{'table': path+'genes_expression_100_Leanne.txt','columns': "all"})
-
-    def test_pca_rpkm(self):
-        self.plugin(**{'table': path+'genes_expression_100_Leanne.txt','columns': "rpkm"})
 
     def tearDown(self):
         for f in os.listdir('.'):
