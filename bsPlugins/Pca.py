ac8cc0ce439c87e7ef738d65cd8d648fc4179389
Julien Delafontaine <julien.delafontaine@epfl.ch>
2015-01-20 16:48:38 +0100

    doc

--- bsPlugins/Pca.py
+++ bsPlugins/Pca.py
@@ -16,12 +16,28 @@ class PcaForm(BaseForm):
         validator=twb.BsFileFieldValidator(required=True))
     columns = twf.TextField(label='Columns selection: ',
         value="all",
-        help_text='Which columns? "all"/"rpkm" (name contains rpkm)/"1,3,5,6,..."')
+        help_text='Which columns? "all"/"1,3,5,6"')
     submit = twf.SubmitButton(id="submit", value="Plot")
 
 
 class PcaPlugin(BasePlugin):
-    """Generates a PCA biplot for diagnostic."""
+    """Generates a PCA biplot for diagnostic.
+
+  The intput is a tab-delimited table with feature names in the first column,
+  then one column of respective scores per sample.
+  The first line is a header of the type "ID  sample1  sample2 ...".
+  Ideal relevant column names are of the form
+  "[prefix.]<group_name>.<#replicate>", e.g. "rpkm.Control.2" or "TRF2_KO.1",
+  so that the plugin will recognize the group name and replicate number:
+  all samples with a different group name will get a different color.
+
+  Example of input:
+
+    ID    rpkm.KO.1    rpkm.KO.2    rpkm.WT.1
+    AAA   9.0          8.1          0.0
+    BBB   3.2          9.0          7.4
+
+    """
     info = {'title': 'PCA',
             'description': __doc__,
             'path': ['Graphics', 'PCA'],
