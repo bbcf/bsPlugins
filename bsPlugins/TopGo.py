b7f81e11f92f53fd9e3470f8d4eb3385ddd924fe
Jacques Rougemont <jacques.rougemont@epfl.ch>
2016-04-28 09:29:44 +0200

    yeast name changed

--- bsPlugins/TopGo.py
+++ bsPlugins/TopGo.py
@@ -4,7 +4,7 @@ import os, tarfile
 
 mart_map = [("GRCh37",'hg19'), ("GRCh38",'hg38'), 
             ("NCBIM37",'mm9'), ("GRCm38",'mm10'), 
-            ("EF3","sacCer2"), ("BDGP5",'dm3'),
+            ("EF4","sacCer2"), ("BDGP5",'dm3'),
             ("Zv9",'zv9')]
 
 default_path = "/mnt/common/epfl/share"
